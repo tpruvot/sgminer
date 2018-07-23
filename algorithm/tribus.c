ca3c79363afdb9f327e00e0b102b1ff520e77793
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2018-07-23 08:30:05 +0200

    gcc 7.3 fix for tribus + submodule branch

--- algorithm/tribus.c
+++ algorithm/tribus.c
@@ -60,10 +60,7 @@ be32enc_vect(uint32_t *dst, const uint32_t *src, uint32_t len)
 }
 
 
-#ifdef __APPLE_CC__
-static
-#endif
-inline void tribushash(void *state, const void *input)
+static inline void tribushash(void *state, const void *input)
 {
     uint32_t hash[16];
 
